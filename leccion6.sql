ae345ad22a8c3de96ee85702eef345f6a8dfeae5
maria <mpramos78915400@gmail.com>
2024-03-27 20:52:21 -0400

    segundo ejercicio leccion 6

--- leccion6.sql
+++ leccion6.sql
@@ -5,3 +5,14 @@ select P.titulo as pelicula,
 	T.ventas_locales, T.ventas_internacionales
 from movie as P
 JOIN taquilla as T on P.identficacion=T.id_pelicula
+
+
+--? Muestre las cifras de ventas de cada película que obtuvo mejores resultados a nivel internacional que a nivel nacional.
+select P.titulo as pelicula,
+	T.ventas_locales, T.ventas_internacionales
+from movie as P
+JOIN taquilla as T on P.identficacion=T.id_pelicula
+where T.ventas_internacionales > T.ventas_locales
+
+--? Enumere todas las películas por clasificación en orden descendente
+
